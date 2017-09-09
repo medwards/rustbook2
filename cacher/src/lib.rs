041e9b962d9c1d2c074ee26a37dd88479780f8b0
Michael Edwards <medwards@walledcity.ca>
2017-09-09 12:43:53 +0200

    Ch. 13 - Cacher Exercise - support mismatched input/outputs

--- cacher/src/lib.rs
+++ cacher/src/lib.rs
@@ -1,34 +1,37 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
-struct Cacher<'a, T, U, V>
-    where T: Fn(&'a U) -> V,
-          U: 'a + Eq + Hash
+struct Cacher<T, U, V>
+    where T: Fn(U) -> V,
+          U: Eq + Hash + Copy,
+          V: Copy
 {
     calculation: T,
-    values: HashMap<&'a U, V>
+    values: HashMap<U, V>
 }
 
-impl<'a, T, U, V> Cacher<'a, T, U, V>
-    where T: Fn(&'a U) -> V,
-          U: 'a + Eq + Hash
+impl<T, U, V> Cacher<T, U, V>
+    where T: Fn(U) -> V,
+          U: Eq + Hash + Copy,
+          V: Copy
 {
-    fn new(calculation: T) -> Cacher<'a, T, U, V> {
+    fn new(calculation: T) -> Cacher<T, U, V> {
         Cacher {
             calculation,
             values: HashMap::new(),
         }
     }
 
-    fn value(&mut self, arg: &U) -> &V {
-        match self.values.get(arg) {
-            Some(ref v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.values.insert(arg, v);
-                &v
-            }
+    fn value(&mut self, arg: U) -> V {
+        // Two-part match here is forced by borrow checker
+        // self.values immutable borrow in the match, mutable borrow in the None arm
+        match self.values.get(&arg) {
+            Some(v) => return *v,
+            None => (),
         }
+        let v = (self.calculation)(arg);
+        self.values.insert(arg, v);
+        v
     }
 }
 
@@ -40,7 +43,7 @@ mod tests {
     fn call_with_cache_hit() {
         let mut c = Cacher::new(|a| a);
 
-        let v1 = c.value(&1);
+        let v1 = c.value(1);
 
         assert_eq!(v1, 1);
     }
@@ -61,16 +64,31 @@ mod tests {
 
         let v1 = c.value("asfd");
 
-        assert_eq!(*v1, "asfd");
+        assert_eq!(v1, "asfd");
     }
 
     #[test]
     fn call_with_different_inputs_outputs() {
-        let mut c = Cacher::new(|a: &String| -> usize { a.len() });
+        let mut c = Cacher::new(|a: &str| -> usize { a.len() });
 
-        let val = "asfd".to_string();
-        let v1 = c.value(&val);
+        let v1 = c.value("asfd");
 
-        assert_eq!(v1, 4 as uszie);
+        assert_eq!(v1, 4 as usize);
+    }
+
+    #[test]
+    fn call_with_string_input() {
+        let a1 = String::from("asdf");
+        let v1 = {
+            let mut c = Cacher::new(|a: &str| -> usize { a.len() });
+            c.value(&a1[..])
+        };
+
+        assert_eq!(v1, 4 as usize);
+
+        // Switch to Copy to Clone and add clone basically every time arg and v are used
+        //let mut c = Cacher::new(|a: String| -> usize { a.len() });
+        //let v1 = c.value(String::from("asfd"));
+        //assert_eq!(v1, 4 as usize);
     }
 }
